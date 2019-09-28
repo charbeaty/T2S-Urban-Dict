1f655022dd70d6cc72d76921fc5fa6da4cbba89b
Randamus <rcviets@gmail.com>
2019-09-28 12:54:33 -0600

    button click/ajax pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,21 @@ var settings = {
 $.ajax(settings).done(function (response) {
     console.log(response);
 });
+
+//Variable to hold recently searched
+var recentlySearched = [];
+
+//Button Click for recently searched buttons
+$(document).on('click', '#topic-buttons', function() {
+    $('#definition-view').empty();
+
+    ///PLACEHOLDERS
+var thisSearch = $(this).attr('PLACEHOLDER');
+var queryURL = 'PLACEHOLDER' + thisSearch + 'PLACEHOLDER'
+
+//AJAX GET
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+})
+});
